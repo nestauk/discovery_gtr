7469065d65c2bc7f1fcd494fe7a4d2d0edf977e5
tomwillcocks1 <thomas.willcocks@hotmail.com>
2023-12-18 14:51:41 +0000

    moved endpoints to variables; attempted to parallelise all endpoint calls

--- discovery_gtr/getters/gtr_to_s3.py
+++ discovery_gtr/getters/gtr_to_s3.py
@@ -17,6 +17,8 @@ import boto3
 import os
 from dotenv import load_dotenv
 import io
+import sys
+import math
 
 
 # Set up environment variables, either from .env file or GitHub secrets. This includes:
@@ -38,38 +40,22 @@ AWS_SECRET_KEY = os.getenv("AWS_SECRET_KEY")
 # Retrieve S3 bucket name and destination path from environment variables
 MY_BUCKET_NAME = os.getenv("MY_BUCKET_NAME")
 DESTINATION_S3_PATH = os.getenv("DESTINATION_S3_PATH")
+# Retrieve the endpoint from the environment variable
+ENDPOINT = os.getenv("ENDPOINT")
 
-
-# Set up logging and set desired logging level
-logging.basicConfig(level=logging.INFO)
-
-# Define the API endpoints
-ENDPOINTS = [
-    "funds",
-    "organisations",
-    "outcomes",
-    "outcomes/keyfindings",
-    "outcomes/impactsummaries",
-    "outcomes/publications",
-    "outcomes/collaborations",
-    "outcomes/intellectualproperties",
-    "outcomes/policyinfluences",
-    "outcomes/products",
-    "outcomes/researchmaterials",
-    "outcomes/spinouts",
-    "outcomes/furtherfundings",
-    "outcomes/disseminations",
-    "persons",
-    "projects",
-]
 
 # Define the API URL
 BASE_URL = "https://gtr.ukri.org/gtr/api/"
+
 
 # Initialize the S3 client with credentials
 S3 = boto3.client(
     "s3", aws_access_key_id=AWS_ACCESS_KEY, aws_secret_access_key=AWS_SECRET_KEY
 )
+
+
+# Set up logging and set desired logging level
+logging.basicConfig(level=logging.INFO)
 
 
 def main_request(base_url: str, endpoint: str, page_parameter: str = ""):
@@ -91,7 +77,7 @@ def get_total_pages(response) -> int:
         response: The response object.
     Returns:
         The total number of pages."""
-    # Parse the JSON response
+    logging.info("Parsing total number of pages from API response")
     json_response = json.loads(response)
 
     # Extract the total number of pages
@@ -106,10 +92,11 @@ def get_page_range(total_pages: int) -> range:
         total_pages (int): The total number of pages.
     Returns:
         A range of integers."""
+    logging.info(f"Generating page range from 1 to {total_pages}")
     return range(1, total_pages + 1)
 
 
-def content_type(response, endpoint) -> None:
+def content_type(response, endpoint: str) -> None:  # Not used
     """
     Prints the content type for each endpoint.
     Args:
@@ -117,13 +104,13 @@ def content_type(response, endpoint) -> None:
     Returns:
         None
     """
-    # Check the content type
+    logging.info(f"Checking content type for endpoint: {endpoint}")
     content_type = response.headers["content-type"]
     # Print
     logging.info(f"{content_type}: Content Type for {endpoint}")
 
 
-def get_s3_key(name, destination_path) -> str:
+def get_s3_key(name: str, destination_path: str) -> str:
     """
     Generates the S3 key for the file.
     Args:
@@ -132,16 +119,15 @@ def get_s3_key(name, destination_path) -> str:
     Returns:
         str: The S3 key for the given file.
     """
-    # Define the timestamp
+    logging.info("Generating S3 key for the file")
     now = datetime.datetime.now()
-    # Format timestamp as YYYYMMDD_HHMMSS
     timestamp = now.strftime("%Y%m%d_%H%M%S")
-
-    # Return the S3 key with the directory and file name
     return f"{destination_path}GtR_{timestamp}/gtr_{name}.csv"
 
 
-def upload_data_to_s3(data, s3_client, bucket_name, s3_key) -> None:
+def upload_data_to_s3(
+    data: list, s3_client: str, bucket_name: str, s3_key: str
+) -> None:
     """Upload data to a JSON file stored on S3.
     Args:
         data (list): The list of data to upload.
@@ -150,10 +136,8 @@ def upload_data_to_s3(data, s3_client, bucket_name, s3_key) -> None:
         s3_key (str): The S3 key.
     Returns:
         None"""
-    # Serialize the data to JSON
+    logging.info(f"Uploading data to S3: {s3_key}")
     json_data = json.dumps(data, ensure_ascii=False, separators=(",", ":"))
-
-    # Upload JSON content to S3 using s3.put_object()
     s3_client.put_object(Body=json_data.encode("utf-8"), Bucket=bucket_name, Key=s3_key)
 
 
@@ -163,6 +147,7 @@ def gtr_to_s3(endpoint: str) -> None:
         endpoint (str): The endpoint to call.
     Returns:
         None"""
+    logging.info(f"Starting workflow for endpoint: {endpoint}")
     # Get the total number of pages
     r = main_request(BASE_URL, endpoint)
     total_pages = get_total_pages(r.content)
@@ -176,16 +161,24 @@ def gtr_to_s3(endpoint: str) -> None:
     # Accumulate data for all pages
     all_data = []
 
+    # Initialize the previously logged percentage
+    prev_percentage = None
+
     for page_no in range:
         # Fetch the page
         r = main_request(BASE_URL, endpoint, f"&p={page_no}")
         # Accumulate data
         all_data.extend(r.json())
 
-        # Log the percentage of completion when it reaches a whole number
+        # Log the percentage of completion when it reaches a milestone
         percentage_complete = (page_no / total_pages) * 100
-        if percentage_complete.is_integer():
-            logging.info(f"{endpoint}: Downloaded {percentage_complete:.0f}%")
+        rounded_percentage = math.floor(percentage_complete)
+
+        # Check if the rounded percentage has changed
+        if rounded_percentage != prev_percentage:
+            logging.info(f"{endpoint}: Downloaded {rounded_percentage}%")
+            # Update the previously logged percentage
+            prev_percentage = rounded_percentage
 
     # Upload all data to S3 using s3.put_object()
     upload_data_to_s3(all_data, S3, MY_BUCKET_NAME, s3_key)
@@ -193,76 +186,11 @@ def gtr_to_s3(endpoint: str) -> None:
 
 # Run the workflow
 if __name__ == "__main__":
-    gtr_to_s3(ENDPOINTS[0])
-
-
-# Old code
-
-
-def append_to_s3(data, s3_client, bucket_name, s3_key) -> None:
-    """Append data to a JSON file stored on S3.
-    Args:
-        data (json): The data to append.
-        s3_client (str): The S3 client.
-        bucket_name (str): The name of the bucket.
-        s3_key (str): The S3 key.
-    Returns:
-        None"""
-    # Create a StringIO object to store JSON content as a string
-    json_content_buffer = io.StringIO()
-
-    # Check if the file already exists on S3
-    try:
-        # Download existing JSON content from S3
-        existing_content = (
-            s3_client.get_object(Bucket=bucket_name, Key=s3_key)["Body"]
-            .read()
-            .decode("utf-8")
+    logging.info("Script execution started")
+    if endpoint:
+        gtr_to_s3(ENDPOINT)
+    else:
+        logging.error(
+            "Endpoint not specified. Please set the ENDPOINT environment variable."
         )
-        existing_data = json.loads(existing_content)
-    except s3_client.exceptions.NoSuchKey:
-        # If the key does not exist, create an empty list
-        existing_data = []
-
-    # Append the new data to the existing data
-    existing_data.extend(data)
-
-    # Serialize the updated data to JSON directly to the buffer
-    json.dump(
-        existing_data,
-        json_content_buffer,
-        ensure_ascii=False,
-        indent=None,
-        separators=(",", ":"),
-    )
-
-    # Seek to the beginning of the buffer
-    json_content_buffer.seek(0)
-
-    # Upload JSON content to S3 using s3.put_object()
-    s3_client.put_object(
-        Body=json_content_buffer.read().encode("utf-8"), Bucket=bucket_name, Key=s3_key
-    )
-
-
-def gtr_to_s3_paginatedupload(endpoint: str) -> None:
-    """Fetches a paginated bulk data resource from GtR API, and saves it to S3.
-    Args:
-        endpoint (str): The endpoint to call.
-    Returns:
-        None"""
-    # Get the total number of pages
-    r = main_request(BASE_URL, endpoint)
-    total_pages = get_total_pages(r.content)
-    logging.info(f"{total_pages}: Total number of pages for {endpoint}")
-    # Get the page range
-    range = get_page_range(total_pages)
-
-    # Get S3 key
-    s3_key = get_s3_key(endpoint, DESTINATION_S3_PATH)
-
-    for page_no in range:
-        # Fetch the page
-        r = main_request(BASE_URL, endpoint, f"&p={page_no}")
-        # Upload file to S3 using s3.put_object()
-        append_to_s3(r.json(), S3, MY_BUCKET_NAME, s3_key)
+    logging.info("Script execution completed")
---
+++ discovery_gtr/getters/unused_code.py
@@ -0,0 +1,70 @@
+# Old code
+
+
+def append_to_s3(data, s3_client, bucket_name, s3_key) -> None:
+    """Append data to a JSON file stored on S3.
+    Args:
+        data (json): The data to append.
+        s3_client (str): The S3 client.
+        bucket_name (str): The name of the bucket.
+        s3_key (str): The S3 key.
+    Returns:
+        None"""
+    # Create a StringIO object to store JSON content as a string
+    json_content_buffer = io.StringIO()
+
+    # Check if the file already exists on S3
+    try:
+        # Download existing JSON content from S3
+        existing_content = (
+            s3_client.get_object(Bucket=bucket_name, Key=s3_key)["Body"]
+            .read()
+            .decode("utf-8")
+        )
+        existing_data = json.loads(existing_content)
+    except s3_client.exceptions.NoSuchKey:
+        # If the key does not exist, create an empty list
+        existing_data = []
+
+    # Append the new data to the existing data
+    existing_data.extend(data)
+
+    # Serialize the updated data to JSON directly to the buffer
+    json.dump(
+        existing_data,
+        json_content_buffer,
+        ensure_ascii=False,
+        indent=None,
+        separators=(",", ":"),
+    )
+
+    # Seek to the beginning of the buffer
+    json_content_buffer.seek(0)
+
+    # Upload JSON content to S3 using s3.put_object()
+    s3_client.put_object(
+        Body=json_content_buffer.read().encode("utf-8"), Bucket=bucket_name, Key=s3_key
+    )
+
+
+def gtr_to_s3_paginatedupload(endpoint: str) -> None:
+    """Fetches a paginated bulk data resource from GtR API, and saves it to S3.
+    Args:
+        endpoint (str): The endpoint to call.
+    Returns:
+        None"""
+    # Get the total number of pages
+    r = main_request(BASE_URL, endpoint)
+    total_pages = get_total_pages(r.content)
+    logging.info(f"{total_pages}: Total number of pages for {endpoint}")
+    # Get the page range
+    range = get_page_range(total_pages)
+
+    # Get S3 key
+    s3_key = get_s3_key(endpoint, DESTINATION_S3_PATH)
+
+    for page_no in range:
+        # Fetch the page
+        r = main_request(BASE_URL, endpoint, f"&p={page_no}")
+        # Upload file to S3 using s3.put_object()
+        append_to_s3(r.json(), S3, MY_BUCKET_NAME, s3_key)
